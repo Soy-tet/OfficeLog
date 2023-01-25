fdbc623adc0caa9eb54d7457cda19374796561df
BLUENI <114486018+Soy-tet@users.noreply.github.com>
2023-01-25 19:22:40 +0700

    javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+function sting (){
+    var x = window.screen.availWidth;
+    if(x<=500){
+        var u = document.getElementById('happy');
+        u.setAttribute("style","display","block");
+    }
+}
+window.screen.availHeight=sting()
